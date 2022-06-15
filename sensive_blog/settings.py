858a63538a07f52c7aee29622f4299ecdfd54275
MSMikl <77015283+MSMikl@users.noreply.github.com>
2022-06-15 13:47:49 +0300

    static files location changed

--- sensive_blog/settings.py
+++ sensive_blog/settings.py
@@ -95,11 +95,8 @@ USE_TZ = True
 STATIC_URL = os.getenv('STATIC_URL', '/static/')
 
 STATICFILES_DIRS = [
-    (os.path.join(BASE_DIR, "static")),
-    (os.path.join(BASE_DIR, "img")),
-    (os.path.join(BASE_DIR, "fonts"))
+    (os.path.join(BASE_DIR, "static"))
     ]
-
 
 MEDIA_ROOT = os.getenv("MEDIA_ROOT", os.path.join(BASE_DIR, 'media'))
 
